d9d603b1863aed2edd20ea7a31ce96482d766087
ThomasEmilsson <thomasemilsson10@gmail.com>
2016-09-23 08:54:09 +0200

    Initial Commit

---
+++ Baldr/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Baldr
+//
+//  Created by Thomas Emilsson on 9/23/16.
+//  Copyright © 2016 Thomas Emilsson. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
