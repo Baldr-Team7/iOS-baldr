866c00dcc7e432ec8511858adb0008906ee99754
ThomasEmilsson <thomasemilsson10@gmail.com>
2016-10-02 00:22:16 +0200

    Lights Table Cell UI + Lights Table Cell class. Testing

---
+++ Baldr/LightsTableCell.swift
@@ -0,0 +1,25 @@
+//
+//  LightsTableCell.swift
+//  Baldr
+//
+//  Created by Thomas Emilsson on 10/2/16.
+//  Copyright © 2016 Thomas Emilsson. All rights reserved.
+//
+
+import UIKit
+
+class LightsTableCell: UITableViewCell {
+
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
