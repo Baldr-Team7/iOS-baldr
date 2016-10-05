303976ef2a9d1c472a40d2e4ed6ec09f0132709c
Zhanou Liang <gusliazh@student.gu.se>
2016-10-05 14:55:11 +0200

    ignore

--- Baldr/MoodsTableViewController.swift
+++ Baldr/MoodsTableViewController.swift
@@ -14,6 +14,7 @@ class MoodsTableViewController: UITableViewController {
     //       ADDMOODVIEW
     //       EDITMOODVIEW
     //       TEST MOODS DELETE/APPEND
+    //       Create IBOutlet
     
     override func viewDidLoad() {
         super.viewDidLoad()
