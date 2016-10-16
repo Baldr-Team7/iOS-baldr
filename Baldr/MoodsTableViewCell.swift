9459eb98fef02dc8dabe82cea351a11edac3ead4
ThomasEmilsson <thomasemilsson10@gmail.com>
2016-10-16 13:10:00 +0200

    Adding Mood Cell Files

---
+++ Baldr/MoodsTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  MoodsTableViewCell.swift
+//  Baldr
+//
+//  Created by Thomas Emilsson on 10/16/16.
+//  Copyright © 2016 Thomas Emilsson. All rights reserved.
+//
+
+import UIKit
+
+class MoodsTableViewCell: UITableViewCell {
+
+  
+    @IBOutlet weak var mainLabel: UILabel!
+    @IBOutlet weak var moodSwitch: UISwitch!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
